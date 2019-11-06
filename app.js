c694d5bd13039a99220e7197f12bab9a3c8c358a
Fahrul Rozi <fahrul.rozi@citra.digital>
2019-11-06 07:11:05 +0700

    another

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-for (let index = 0; index < 5; index++) {
-    console.log(index);
+const data = [{
+    name: 'Fahrul',
+    age: '19'
+},{
+    name: 'Rozi',
+    age: '19'
+}]
+
+const arr = [];
+for (let i = 0; i < data.length; i++) {
+    arr.push(data[i])
 }
+
+console.log(arr)
