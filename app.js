04f7bc4b793fbfd9f6d31c2305a39594c6dac4cb
Fahrul Rozi <fahrul.rozi@citra.digital>
2019-11-07 10:56:23 +0700

    tesst

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-const data = [{
-    name: 'Fahrul',
-    age: '19'
-},{
-    name: 'Rozi',
-    age: '19'
-}]
 
-const arr = [];
-for (let i = 0; i < data.length; i++) {
-    arr.push(data[i])
-}
-
-console.log(arr)
